f2dd386d301c300e3f12ca688a35002b193739d2
Chanchal Junare <chanchaljunare28@gmail.com>
2023-04-06 17:20:55 -0900

    Admin Password Page updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import './styles/index.css';
+
 import Login from './Login';
 import Main from './components/Main'
 import EditItem from './components/Items/EditItem';
