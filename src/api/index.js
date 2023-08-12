071dee48b55bec6d8098ae3519f494650db6c6b2
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2023-08-12 12:41:17 +0530

    changes to toml

--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,10 @@ const url = process.env.REACT_APP_BASE_URL;
 
 // CRUD APIs
 export const getItems = async (id) => await axios.get(`${url}/${id}`);
-export const getItem = async (id) =>  await axios.get(`${url}/getedit${id}`);
-export const donwloadExcelSheet = async (user) => axios.post(`${url}/export`, user,{
+export const getItem = async (id) => await axios.get(`${url}/getedit${id}`);
+export const donwloadExcelSheet = async (user) => axios.post(`${url}/downloadexcel`, user, {
    responseType: 'arraybuffer'
- });
+});
 
 
 export const createItem = async (newItem) => await axios.post(`${url}/add`, newItem);
@@ -23,7 +23,7 @@ export const loginUser = async (user) => await axios.post(`${url}`, user);
 export const getPassword = async (id, password) => await axios.post(`${url}/password${id}`, password);
 export const updatePassword = async (id, updatedPassword) => await axios.put(`${url}/password${id}`, updatedPassword);
 export const forgotPassword = async (id, token) => await axios.get(`${url}/forgotpassword${id}/${token}`);
-export const sendPasswordLink = async (email) =>  await axios.post(`${url}/sendpasswordlink`, email);
+export const sendPasswordLink = async (email) => await axios.post(`${url}/sendpasswordlink`, email);
 export const setNewPassword = async (id, token, password) => await axios.post(`${url}/${id}/${token}`, password);
 export const setUserPasswordByAdmin = async (user) => await axios.post(`${url}/setuserpassword`, user);
 export const deleteAllData = async (id) => await axios.delete(`${url}/deletedata${id}`);
